31e16399b2cafb2909a2c20691df78998c45c4bc
Fabian Luna <flb031@gmail.com>
2019-03-12 13:53:41 -0500

    fix: endpoint provider list

--- app/routes/provider.js
+++ app/routes/provider.js
@@ -6,7 +6,7 @@ var router = express.Router()
 var ProviderController = require('../controllers/provider')
 var mdAuth = require('../middlewares/authenticated')
 
-router.get('/:page?', ProviderController.index )
+router.get('/page/:page', ProviderController.index )
 router.post('/', mdAuth.ensureAuth, ProviderController.create )
 router.get('/:id', ProviderController.show )
 router.put('/:id', mdAuth.ensureAuth, ProviderController.update )
